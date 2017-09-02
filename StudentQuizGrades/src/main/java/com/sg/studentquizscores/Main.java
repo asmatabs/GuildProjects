5ae41759849129677e948d367ab9f8642c65c6bf
Asma Tabassum <asma.tabs@gmail.com>
2017-09-02 12:47:38 -0500

    .

--- StudentQuizGrades/src/main/java/com/sg/studentquizscores/Main.java
+++ StudentQuizGrades/src/main/java/com/sg/studentquizscores/Main.java
@@ -5,46 +5,58 @@
  */
 package com.sg.studentquizscores;
 
+import java.io.IOException;
+
 /**
  *
  * @author asmat
  */
 public class Main {
-    
-    public static void main(String[] args) {
+
+    public static void main(String[] args) throws IOException {
 
         UserIO io = new UserIOImpl();
-        StudentInfo studentInfo = new StudentInfo();
-        boolean exitPortal = false;
-        do{
-        int option = menu();
-        switch(option)
-        {
-            case 1:
-                studentInfo.listStudents();
-                break;
-            case 2:
-                studentInfo.addStudent();
-                break;
-            case 3: 
-                studentInfo.removeStudent(io.readString("Enter student name:"));
-                break;
-            case 4:
-                studentInfo.studentQuizScores(io.readString("Enter student name:"));
-                break;
-            case 5: 
-                studentInfo.avgStudentQuizScores(io.readString("Enter student name:"));
-                break;
-            default:
-                exitPortal = true;
-        }
-    }while(!exitPortal);
+        io.print("Welcome to the Student portal");
         
+        StudentInfo studentInfo = new StudentInfo();
+
+        //Unmarshall and load the map here!!
+        studentInfo.loadData();
+        boolean exitPortal = false;
+        do {
+            int option = menu();
+            switch (option) {
+                case 1:
+                    studentInfo.listStudents();
+                    break;
+                case 2:
+                    studentInfo.addStudent();
+                    break;
+                case 3:
+                    studentInfo.removeStudent(io.readString("Enter student name:"));
+                    break;
+                case 4:
+                    studentInfo.studentQuizScores(io.readString("Enter student name:"));
+                    break;
+                case 5:
+                    studentInfo.avgStudentQuizScores(io.readString("Enter student name:"));
+                    break;
+                default:
+                    exitPortal = true;
+            }
+        } while (!exitPortal);
+
+        //Marshall the data
+        io.print("Please wait while the changes are saved...");
+        studentInfo.saveChanges();
+
+        io.print("Thanks for using the Student portal!");
+
     }
 
     public static int menu() {
         UserIO io = new UserIOImpl();
-        io.print("Welcome to the Student portal");
+
         io.print("Please select from the following options...");
 
         io.print("1 - List Students");
@@ -53,8 +65,8 @@ public class Main {
         io.print("4 - View quiz scores for a student");
         io.print("5 - View average scores for a student");
         io.print("Any other key to exit the portal..");
-        
+
         return io.readInt("Your Choice: ");
     }
- 
+
 }
--- StudentQuizGrades/src/main/java/com/sg/studentquizscores/StudentInfo.java
+++ StudentQuizGrades/src/main/java/com/sg/studentquizscores/StudentInfo.java
@@ -5,8 +5,17 @@
  */
 package com.sg.studentquizscores;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.HashMap;
+import java.util.Scanner;
 import java.util.Set;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import java.util.stream.IntStream;
 
 /**
@@ -15,15 +24,21 @@ import java.util.stream.IntStream;
  */
 public class StudentInfo {
 
-    int[] scores = new int[5];
-    HashMap<String, int[]> quizScores = new HashMap<>();
+    int[] scores;
+    HashMap<String, int[]> quizScores;
     UserIO io = new UserIOImpl();
     public final int SUBJECTS = 5;
 
+    public StudentInfo()
+    {
+       quizScores = new HashMap<>(); 
+       io = new UserIOImpl();
+       scores = new int[5];
+    }
     public void listStudents() {
         Set<String> students = quizScores.keySet();
         for (String s : students) {
-            io.print("Student Name: " + s.toUpperCase());
+            io.print("Student Name: " + s);
         }
     }
 
@@ -58,5 +73,52 @@ public class StudentInfo {
         int[] scores = quizScores.get(studentName);
         int avg = IntStream.of(scores).sum() / SUBJECTS;
         io.print("Quiz scores - " + avg);
+    }
+
+    public void loadData() throws FileNotFoundException {
+        Scanner sc = new Scanner(
+                new BufferedReader(new FileReader("OutFile.txt")));
+        System.out.println("Opening file to load data...");
+        // go through the file line by line
+        while (sc.hasNextLine()) {
+            System.out.println("Found a record to load");
+            String currentLine = sc.nextLine();
+            
+            String delim = "::";
+            String[] split = currentLine.split(delim);
+            System.out.println(split[0]);
+            String studentName = split[0];
+            int[] scores = new int[5];
+            for (int i = 1; i < split.length; i++)
+            {
+                scores[i-1] = Integer.valueOf(split[i]);
+            }
+            quizScores.put(studentName, scores);
+        }
+    }
+
+    public void saveChanges() throws IOException {
+        try {
+            PrintWriter out = new PrintWriter(new FileWriter("OutFile.txt"));
+
+            Set<String> students = quizScores.keySet();
+            for (String s : students) {
+                System.out.println("Saving " + s);
+                out.print(s);
+                int[] scores = quizScores.get(s);
+                for (int i = 0; i < scores.length; i++) {
+                    out.print("::" + scores[i]);
+                }
+                out.println();
+                // out.print(quizScores.get(s));
+            }
+
+            out.flush();
+            out.close();
+        } catch (IOException ex) {
+            Logger.getLogger(StudentInfo.class.getName()).log(Level.SEVERE, null, ex);
+
+        }
+
     }
 }
